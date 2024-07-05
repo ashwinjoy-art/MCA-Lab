ec8e998fb61f5c4783221650c9b6867cd92d0e8b
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-07-05 12:22:07 +0530

    KTU MCA S2 Lab

--- S2/OOPs/MatrixAddition.java
+++ S2/OOPs/MatrixAddition.java
@@ -13,19 +13,23 @@ public class MatrixAddition
         int[][] matrix2 = new int[rows][columns];
         int[][] sumMatrix = new int[rows][columns];
         System.out.println("Enter the elements of the first matrix:");
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++) 
+        {
             for (int j = 0; j < columns; j++) 
             {
                 matrix1[i][j] = scanner.nextInt();
             }
         }
+
         System.out.println("Enter the elements of the second matrix:");
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++) 
+        {
             for (int j = 0; j < columns; j++) 
             {
                 matrix2[i][j] = scanner.nextInt();
             }
         }
+
         for (int i = 0; i < rows; i++) 
         {
             for (int j = 0; j < columns; j++) 
@@ -33,6 +37,7 @@ public class MatrixAddition
                 sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
+
         System.out.println("Sum of the matrices:");
         for (int i = 0; i < rows; i++) 
         {
